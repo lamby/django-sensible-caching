fcc99f26b6853601c7a47f873ba7256e698e019e
Chris Lamb <chris@chris-lamb.co.uk>
2015-10-07 09:50:49 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-sensible-caching',
-    description="Non-magical object caching for Django.",
+
+    url="https://chris-lamb.co.uk/projects/django-sensible-caching",
     version='1',
+    description="Non-magical object caching for Django.",
+
+    author="Chris Lamb",
+    author_email="chris@chris-lamb.co.uk",
+    license="BSD",
+
     packages=find_packages(),
 )
